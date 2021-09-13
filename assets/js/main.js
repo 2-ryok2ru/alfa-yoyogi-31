4e177dcba2c13c7ac884e6ffed7ea4c03f6564db
2-ryok2ru <89884208+2-ryok2ru@users.noreply.github.com>
2021-09-13 10:07:06 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
           q1: 'YOKO',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'RIGHT',
           // q2: 'えええ',
           // q3: 'おおお'
         },
